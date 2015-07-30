b7365315812db2b3d7a1382bc1252d5b724c616c
Matt Watson <mattwatson@Matts-MacBook-Pro.local>
2015-07-30 21:19:02 +0100

    Restored Grunt File

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-config')(grunt, {
+      init: true,
+      jitGrunt: {
+          jitGrunt: true,
+          staticMappings: {
+            scsslint: 'grunt-scss-lint'
+          }
+      },
+      data: {
+
+        wpInfo: {
+    		  wp_theme_name: 'cpd-theme',
+    		  wp_content: 'htdocs/wp-content'
+        },
+
+        wpPlugins: [
+          '<%= wpInfo.wp_content %>/plugins/cpd/**/*.php'
+        ],
+
+        concatHead: [
+            'assets/js/lib/modernizr-custom.js',
+            'bower_components/respondJS/dest/respond.src.js',
+            'assets/js/header.js'
+        ],
+
+        concatFoot: [
+            'assets/js/footer.js'
+        ]
+    }
+  });
+  require('time-grunt')(grunt);
+};
